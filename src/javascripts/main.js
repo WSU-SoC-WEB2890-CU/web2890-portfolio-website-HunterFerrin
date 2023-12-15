7efc5654a41aa899325922116111da8a9e3d1482
Hunter Ferrin <hunterferrin@mail.weber.edu>
2023-12-14 19:53:12 -0700

    added formatting to code snippets

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,7 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+// Using require
+const hljs = require('highlight.js/lib/core');
+
+// Load any languages you need
+hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
